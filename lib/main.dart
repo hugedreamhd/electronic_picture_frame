d2a9e98993f8284f74f5bea344b2a48085ceec1c
Sangin Han <hugedreamhd@gmail.com>
2023-12-21 23:02:06 +0900

    Future Builder로 선택후 사진 표시

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -52,6 +54,22 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
       appBar: AppBar(
         title: Text('전자액자'),
       ),
+      body: images == null
+          ? Center(child: Text('No data'))
+          : FutureBuilder<Uint8List>(
+              future: images![0].readAsBytes(),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+
+                if (data == null ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return Image.memory(
+                  data,
+                  width: double.infinity,
+                );
+              }),
     );
   }
 }
