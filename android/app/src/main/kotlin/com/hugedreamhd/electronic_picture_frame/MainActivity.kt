f3ca717c334670e566930af03487c16fd8ba04c8
Sangin Han <hugedreamhd@gmail.com>
2023-12-21 22:19:02 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/hugedreamhd/electronic_picture_frame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hugedreamhd.electronic_picture_frame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyGalleryApp(),
+    );
+  }
+}
+
+class MyGalleryApp extends StatefulWidget {
+  const MyGalleryApp({super.key});
+
+  @override
+  State<MyGalleryApp> createState() => _MyGalleryAppState();
+}
+
+//사진 선택 슬라이드
+class _MyGalleryAppState extends State<MyGalleryApp> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('전자액자'),
+      ),
+    );
+  }
+}
